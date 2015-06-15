36e2a2ee0a11556365cb2c58b4b0a6a722abd8dc
Craig Spaeth <craigspaeth@Craigs-iMac.local>
2015-06-15 16:43:37 -0400

    latest

--- apps/commits/routes.js
+++ apps/commits/routes.js
@@ -2,16 +2,21 @@
 // Routes file that exports route handlers for ease of testing.
 //
 
-var Commits = require('../../collections/commits');
+var Commits = require('../../collections/commits'),
+    request = require('superagent'),
+    API_URL = process.env.API_URL;
 // var commits = new Commits(null, {
 //   owner: 'artsy',
 //   repo: Math.random()
 // });
 
 exports.index = function(req, res, next) {
-  setTimeout(function() {
-    res.render('index', { commits: [] });
-  }, Math.round(500 + Math.random() * 1000));
+  request
+    .get(API_URL + '/repos/artsy/' + Math.random() + '/commits')
+    .timeout(2000)
+    .end(function(err, sres) {
+      res.render('index', { commits: [] });
+    });
   // commits.fetch({
   //   success: function() {
   //     res.locals.sd.COMMITS = commits.toJSON();
