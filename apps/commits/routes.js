07f8636c39c9de963b84021a64bd1f046bb40645
Craig Spaeth <craigspaeth@Craigs-iMac.local>
2015-06-15 20:04:19 -0400

    latest

--- apps/commits/routes.js
+++ apps/commits/routes.js
@@ -4,29 +4,20 @@
 
 var Commits = require('../../collections/commits'),
     request = require('superagent'),
-    API_URL = process.env.API_URL;
-// var commits = new Commits(null, {
-//   owner: 'artsy',
-//   repo: Math.random()
-// });
+    API_URL = process.env.API_URL,
+    supersync = require('../../lib/supersync.js');
 
 exports.index = function(req, res, next) {
-  request
-    .get(API_URL + '/repos/artsy/' + Math.random() + '/commits')
-    .timeout(2000)
-    .end(function(err, sres) {
-      if (err) return res.status(500).send(err);
-      var commits = new Commits(sres.body, {
-        owner: 'artsy',
-        repo: Math.random()
-      });
+  var commits = new Commits(null, {
+    owner: 'artsy',
+    repo: Math.random()
+  });
+  commits.sync = supersync;
+  commits.fetch({
+    success: function() {
+      res.locals.sd.COMMITS = commits.toJSON();
       res.render('index', { commits: commits.models });
-    });
-  // commits.fetch({
-  //   success: function() {
-  //     res.locals.sd.COMMITS = commits.toJSON();
-  //     res.render('index', { commits: commits.models });
-  //   },
-  //   error: function(m, err) { res.status(500).send(err); }
-  // });
+    },
+    error: function(m, err) { res.status(500).send(err); }
+  });
 };
