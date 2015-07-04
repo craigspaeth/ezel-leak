5f8655aa5841845ffddf229137fcb76b43a4f0fb
Craig Spaeth <craigspaeth@Craigs-MacBook-Pro-2.local>
2015-07-04 02:51:57 -0400

    latest

--- ping.js
+++ ping.js
@@ -7,7 +7,7 @@ var superagent = require('superagent'),
     APP_URL = process.env.APP_URL;
 
 var fetch = function (callback) {
-  async.times(400 + Math.round(Math.random() * 400), function(n, next) {
+  async.times(100 + Math.round(Math.random() * 100), function(n, next) {
     console.log('+');
     setTimeout(function() {
       console.log('-');
